a77157ad21a951a242aa63db5eab11214ded7fac
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-04-28 00:33:50 -0600

    dirname edit

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.json())
 
 
 
-app.use(express.static(path.resolve(__dirname, '/client/build')))
+app.use(express.static(path.resolve(__dirname, 'client/build')))
 
 app.use('/api', apiRouter)
 
