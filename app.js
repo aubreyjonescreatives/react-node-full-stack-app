b81b55170fa7da8cb05f3048fcddfe514b76bf9d
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-04-30 04:53:32 -0600

    adjust app.js for deployment

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(compression())
 
 
 
-//app.use(express.static(path.resolve(__dirname, 'client/build')))
+app.use(express.static(path.resolve(__dirname, 'client/build')))
 
 app.use('/api', apiRouter)
 
